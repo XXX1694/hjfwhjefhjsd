dcbd0d0b41e83abfe6c19ac3f3c504ebb4fbfda9
XXX1694 <serikbay.a04@gmail.com>
2023-11-24 14:53:59 +0600

    deleted func

--- main.cpp
+++ main.cpp
@@ -3,15 +3,8 @@
 
 using namespace std;
 
-void myFunc(String in){
-    for(int i = 0 ; i < in.length(); i++){
-
-    }
-}
-
 int main() {
     string a;
     getline(cin, a);
-    myFunc(a);
     return 0;
 }
