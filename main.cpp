6b75e513674da2a42353aab07c678ae19c7148b3
XXX1694 <serikbay.a04@gmail.com>
2023-11-24 14:51:35 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void myFunc(String in){
+    for(int i = 0 ; i < in.length(); i++){
+
+    }
+}
+
+int main() {
+    string a;
+    getline(cin, a);
+    myFunc(a);
+    return 0;
+}
